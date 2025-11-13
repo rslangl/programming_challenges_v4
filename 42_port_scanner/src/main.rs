9a288e9af4048e56dc8cdd3a9347213f8ca915b0
user <fjellape1447@protonmail.com>
2025-11-13 21:00:38 +0100

    chore: rewrite argnames with optional field

--- 42_port_scanner/src/main.rs
+++ 42_port_scanner/src/main.rs
@@ -1,13 +1,21 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
+#[command(version, about)]
 struct Args {
+    #[arg(short = 't', long)]
+    target: String,
+    #[arg(long)]
+    protocol: Option<String>,
+    #[arg(short, long)]
     port: u8
 }
 
 fn main() {
-    
+
     let args = Args::parse();
 
-    println!("Port: {}", args.port);
+    println!("{:?}", args);
+
+
 }
