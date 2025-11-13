45baec2bdcc086caa596843fed31a670b63b4db5
user <fjellape1447@protonmail.com>
2025-11-13 21:55:05 +0100

    chore: do byte size comparison between sent and received to determine success

--- 42_port_scanner/src/main.rs
+++ 42_port_scanner/src/main.rs
@@ -19,7 +19,17 @@ fn main() -> std::io::Result<()> {
 
     println!("{:?}", args);
 
-    let mut stream: TcpStream = TcpStream::connect(args.target + ":" + args.port.to_string().as_str())?;
-    stream.write(&[1])?;
+    let mut stream: TcpStream = match TcpStream::connect(args.target + ":" + args.port.to_string().as_str()) {
+        Ok(ts) => ts,
+        Err(e) => return Err(e)
+    };
+    let buf: &[u8] = &[1];
+    let ret_bytes = match stream.write(buf) {
+        Ok(bytes) => bytes,
+        Err(e) => return Err(e)
+    };
+    if ret_bytes == std::mem::size_of_val(buf) {
+        println!("same amount of bytes returned, so must be working, right?");
+    }
     Ok(())
 }
