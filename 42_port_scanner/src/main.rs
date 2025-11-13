479ae653c6533dc01a20127cd47e499c208b3a49
user <fjellape1447@protonmail.com>
2025-11-13 20:28:09 +0100

    chore: capture port from CLI

--- 42_port_scanner/src/main.rs
+++ 42_port_scanner/src/main.rs
@@ -1,4 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    port: u8
+}
 
 fn main() {
-    println!("hello port scanner");
+    
+    let args = Args::parse();
+
+    println!("Port: {}", args.port);
 }
